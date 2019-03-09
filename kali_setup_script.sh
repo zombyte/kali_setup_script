ff232028a5c0debaa97fd9fe3e47c267441850ad
Zombyte <8269264+zombyte@users.noreply.github.com>
2019-03-09 15:48:36 -0500

    Update kali_setup_script.sh

--- kali_setup_script.sh
+++ kali_setup_script.sh
@@ -40,47 +40,49 @@ fi
 # OS Package Installations
 echo -e "\n== 2.0 OS Package Installations"
 
-echo -e "\n=== 2.1 Installing Docker"
-apt remove docker docker-engine docker.io containerd runc
-apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
-apt update
-apt install -y docker-ce
-systemctl start docker
-systemctl enable docker
-
-echo -e "\n=== 2.2 Installing Snapd"
+echo -e "\n=== 2.1 Installing Snapd"
 apt install -y snapd
 systemctl start snapd
 systemctl enable snapd
 echo -e "export PATH=\$PATH:/snap/bin" >> ~/.bashrc
 source ~/.bashrc
 
-echo -e "\n=== 2.3 Installing GoLang"
+wait
+echo -e "\n=== 2.2 Installing GoLang"
 apt install -y golang
 echo -e "export GOPATH=\$HOME/go" >> ~/.bash_profile
 source ~/.bash_profile
 echo -e "export PATH=\$PATH:\$GOPATH/bin" >> ~/.bashrc
 source ~/.bashrc
 
-echo -e "\n=== 2.4 Installing Node/NPM"
+wait
+echo -e "\n=== 2.3 Installing Node/NPM"
 apt install -y npm
 echo -e "export PATH=\$PATH:/opt/node_modules/.bin" >> ~/.bashrc
 source ~/.bashrc
 
-echo -e "\n=== 2.4 Installing KeepNote"
+wait
+echo -e "\n=== 2.4 Installing PIP3"
+apt install -y python3-pip
+
+wait
+echo -e "\n=== 2.5 Installing KeepNote"
 apt install -y keepnote
 
-echo -e "\n=== 2.5 Installing SecLists"
+wait
+echo -e "\n=== 2.6 Installing SecLists"
 apt install -y seclists
 
-echo -e "\n=== 2.6 Installing GoBuster"
+wait
+echo -e "\n=== 2.7 Installing GoBuster"
 apt install -y gobuster
 
-echo -e "\n=== 2.7 Installing CrackMapExec"
+wait
+echo -e "\n=== 2.8 Installing CrackMapExec"
 apt install -y crackmapexec
 
-echo -e "\n=== 2.8 Installing OpenVAS"
+wait
+echo -e "\n=== 2.9 Installing OpenVAS"
 if ${OPENVAS} ; then
 	apt install -y openvas
 	openvas-setup
@@ -91,40 +93,52 @@ else
 fi
 
 # GIT based repo installations
+wait
 echo "== 3.0 Git Repos (~/Downloads/git_repos)"
 echo -e "\n=== 3.1 Cloning LinEnum"
 git clone https://github.com/rebootuser/LinEnum.git ~/Downloads/git_repos/LinEnum
 
+wait
 echo -e "\n=== 3.2 Cloning linuxprivchecker"
 git clone https://github.com/sleventyeleven/linuxprivchecker.git ~/Downloads/git_repos/linuxprivcehcker
 
+wait
 echo -e "\n=== 3.3 Cloning JAWS"
 git clone https://github.com/411Hall/JAWS.git ~/Downloads/git_repos/JAWS
 
+wait
 echo -e "\n=== 3.4 Cloning unix-privsec-check"
 git clone https://github.com/pentestmonkey/unix-privesc-check.git ~/Downloads/git_repos/unix-privsec-check
 
+wait
 echo -e "\n=== 3.5 Cloning windows-privsec-check"
 git clone https://github.com/pentestmonkey/windows-privesc-check.git ~/Downloads/git_repos/windows-privsec-check
 
+wait
 echo -e "\n=== 3.6 Cloning linux-exploit-suggester"
 git clone https://github.com/mzet-/linux-exploit-suggester.git ~/Downloads/git_repos/linux-exploit-suggester
 
+wait
 echo -e "\n=== 3.7 Cloning Windows-Exploit-Suggester"
 git clone https://github.com/GDSSecurity/Windows-Exploit-Suggester.git ~/Downloads/git_repos/Windows-Exploit-Suggester
 
+wait
 echo -e "\n=== 3.8 Cloning Sherlock"
 git clone https://github.com/rasta-mouse/Sherlock.git ~/Downloads/git_repos/Sherlock
 
+wait
 echo -e "\n=== 3.9 Cloning Watson"
 git clone https://github.com/rasta-mouse/Watson.git ~/Downloads/git_repos/Watson
 
+wait
 echo -e "\n=== 3.10 Cloning php-reverse-shell"
 git clone https://github.com/pentestmonkey/php-reverse-shell.git ~/Downloads/git_repos/php-reverse-shell
 
+wait
 echo -e "\n=== 3.11 Cloning PayloadsAllTheThings"
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git ~/Downloads/git_repos/PayloadsAllTheThings
 
+wait
 echo -e "\n== 3.12 Cloning vulscan"
 mkdir -p /usr/share/nmap/scripts/vulscan
 git clone https://github.com/scipag/vulscan.git ~/Downloads/git_repos/vulscan
@@ -138,6 +152,7 @@ echo "wget http://www.computec.ch/projekte/vulscan/download/xforce.csv  -O /usr/
 echo -e "* 0 * * * root /usr/share/nmap/scripts/vulscan/update.sh" >> /etc/crontab
 /bin/bash /usr/share/nmap/scripts/vulscan/update.sh
 
+wait
 echo -e "\n=== 3.13 Cloning SSRFmap"
 git clone https://github.com/swisskyrepo/SSRFmap.git ~/Downloads/git_repos/SSRFmap
 cd ~/Downloads/git_repos/SSRFmap
@@ -145,11 +160,13 @@ pip3 install -r requirements.txt
 sed -i 's_#!/usr/bin/python_#!/usr/bin/python3_' ~/Downloads/git_repos/SSRFmap/ssrfmap.py 
 ln -s ~/Downloads/git_repos/SSRFmap/ssrfmap.py /usr/bin/ssrfmap
 
+wait
 echo -e "\n=== 3.14 Cloning pacu"
 git clone https://github.com/RhinoSecurityLabs/pacu.git ~/Downloads/git_repos/pacu
 cd ~/Downloads/git_repos
 /bin/bash install.sh
 
+wait
 echo -e "\n=== 3.15 Cloning XSRFProbe"
 git clone https://github.com/0xInfection/XSRFProbe.git ~/Downloads/git_repos/XSRFProbe
 cd ~/Downloads/git_repos/XSRFProbe
@@ -157,6 +174,7 @@ pip3 install -r requirements.txt
 chmod +x ~/Downloads/git_repos/XSRFprobe/xsrfprobe.py
 ln -s ~/Downloads/git_repos/XSRFProbe/xsrfprobe.py /usr/bin/xsrfprobe
 
+wait
 echo -e "\n=== 3.16 Cloning XSStrike"
 git clone https://github.com/s0md3v/XSStrike.git ~/Downloads/git_repos/XSStrike
 cd ~/Downloads/git_repos/XSStrike
@@ -164,11 +182,13 @@ pipr install -r requirements.txt
 chmod +x ~/Downloads/git_repos/XSStrike/xsstrike.py
 ln -s ~/Downloads/git_repos/XSStrike/xsstrike.py /usr/bin/xsstrike
 
+wait
 echo -e "\n=== 3.17 Cloning aws_pwn"
 git clone https://github.com/dagrz/aws_pwn.git ~/Downloads/git_repos/aws_pwn
 cd ~/Downloads/git_repos/aws_pwn
 pip install -r requirements.txt
 
+wait
 echo -e "\n=== 3.18 Cloning subfinder"
 git clone https://github.com/subfinder/subfinder.git ~/Downloads/git_repos/subfinder
 cd ~/Downloads/git_repos/subfinder
@@ -176,6 +196,7 @@ go get
 go build
 ln -s ~/Downloads/git_repos/subfinder/subfinder /usr/bin/subfinder
 
+wait
 echo -e "\n=== 3.19 Cloning ScoutSuite"
 git clone https://github.com/nccgroup/ScoutSuite.git ~/Downloads/git_repos/ScoutSuite
 cd ~/Downloads/git_repos/ScoutSuite
@@ -183,44 +204,37 @@ pip install -r requirements.txt
 chmod +x Scout.py
 ln -s ~/Downloads/git_repos/ScoutSuite/Scount.py /usr/bin/Scout
 
+wait
 echo -e "\n=== 3.20 Cloning NoSQLMap"
 git clone https://github.com/codingo/NoSQLMap.git ~/Downloads/git_repos/NoSQLMap
 cd ~/Downloads/git_repos/NoSQLMap
 python setup.py install
 
-echo -e "\n=== 3.22 Cloning Astra"
-docker pull mongo
-docker run --name astra-mongo -d mongo
-git clone https://github.com/flipkart-incubator/Astra.git ~/Downloads/git_repos/Astra
-cd ~/Downloads/git_repos/Astra
-docker build -t astra .
-docker run --rm -it --link astra-mongo:mongo -p 8094:8094 -d --name astra astra
-git clone -b docker-cli https://github.com/flipkart-incubator/Astra.git ~/Downloads/git_repos/Astra-cli
-cd ~/Downloads/git_repos/Astra-cli
-docker build -t astra-cli .
-docker run --rm -it --link astra-mongo:mongo -d --name astra-cli astra-cli
-
-echo -e "\n=== 3.23 Cloning AutoRecon"
+wait
+echo -e "\n=== 3.21 Cloning AutoRecon"
 git clone https://github.com/Tib3rius/AutoRecon.git ~/Downloads/git_repos/AutoRecon
 cd ~/Downloads/git_repos/AutoRecon
 pip3 install -r requirements.txt
-sed -i "s,'port-scan-profiles.toml','/root/Downloads/git_repos/AutoRecon/port-scan-profiles.toml'," autorecon.py
-sed -i "s,'service-scans.toml','/root/Downloads/git_repos/AutoRecon/service-scans.toml'," autorecon.py
-chmod +x autorecon.py
+sed -i 's\__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))\/root/Downloads/git_repos/AutoRecon\' ~/Downloads/git_repos/AutoRecon/autorecon.py
+chmod +x ~/Downloads/git_repos/autorecon.py
 ln -s ~/Downloads/git_repos/AutoRecon/autorecon.py /usr/bin/autorecon
 
-echo -e "\n=== 3.24 Cloning LFISuite"
+wait
+echo -e "\n=== 3.22 Cloning LFISuite"
 git clone https://github.com/D35m0nd142/LFISuite.git ~/Downloads/git_repos/LFISuite
 
-echo -e "\n=== 3.25 Cloning Empire"
+wait
+echo -e "\n=== 3.23 Cloning Empire"
 git clone https://github.com/EmpireProject/Empire.git ~/Downloads/git_repos/Empire
 cd ~/Downloads/git_repos/Empire/
 ./setup/install.sh
 
-echo -e "\n=== 3.26 Cloning nishang"
+wait
+echo -e "\n=== 3.24 Cloning nishang"
 git clone https://github.com/samratashok/nishang.git ~/Downloads/git_repos/nishang
 
 # Additional Installation Steps
+wait
 echo -e "\n== 4.0 Additional Installations"
 echo -e "\n==== 4.1 Installing PhantomJS, SlimerJS, and CasperJS"
 apt install -y phantomjs
@@ -229,6 +243,7 @@ cd /opt/
 npm install slimerjs
 npm install casterjs
 
+wait
 echo -e "\n=== 4.2 Installing old FireFox for Casper"
 wget https://ftp.mozilla.org/pub/firefox/releases/59.0.3/linux-x86_64/en-US/firefox-59.0.3.tar.bz2 -O /tmp/firefox-59.0.3.tar.bz2
 tar vxjf /tmp/firefox-59.0.3.tar.bz2 -C /opt
@@ -236,32 +251,38 @@ echo "export SLIMERJSLAUNCHER=/opt/firefox/firefox" >> ~/.bash_profile
 source ~/.bash_profile
 rm /tmp/firefox-59.0.3.tar.bz2
 
+wait
 echo -e "\n=== 4.3 Installing wfuzz"
 pip install wfuzz
 
+wait
 echo -e "\n=== 4.4 Installing PyCharm"
 snap install pycharm-community --classic
 
+wait
 echo -e "\n=== 4.5 Installing Atom"
 snap install atom --classic
 
+wait
 echo -e "\n=== 4.6 Set Firefox Bookmarks"
-wget https://github.com/zombyte/kali_setup_script/places.sqlite -O /tmp/places.sqlite
 firefox_default_path=$(find ~/.mozilla/firefox/ -name "places.sqlite")
+kill -s TERM $(pidof firefox-esr)
 sqlite3 ${firefox_default_path} ".restore /tmp/places.sqlite"
 
-# link nc and stuff to git for hosting
-
-wget https://github.com/zombyte/kali_setup_script/host-git.py -O /usr/bin/host-files
+wait
+echo -e "\n=== 4.7 Set up host-files"
+wget https://raw.githubusercontent.com/zombyte/kali_setup_script/master/host-files.py -O /usr/bin/host-files
 chmod +x /usr/bin/host-files
 mkdir -p ~/host-files/os_files
 ln -s /usr/share/windows-binaries/ ~/host-files/os_files/windows-binaries
 ln -s /usr/share/webshells/ ~/host-files/os_files/webshell
-ln -s ~/Downloads/git_repos/ ~/host-files/os_files/webshell
+ln -s ~/Downloads/git_repos/ ~/host-files/git_repos
 
+wait
 echo -e "\n== 5.0 Burp Moditifications"
 
+wait
 echo -e "\n== 6.0 Cleanup"
-apt autoremove -y
-apt autoclean -y
+apt-get autoremove -y
+apt-get autoclean -y
 shutdown -r now
