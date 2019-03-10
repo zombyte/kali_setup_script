c05a9110039ea4a04ec12d18ae7c4dbe60e891b2
Zombyte <8269264+zombyte@users.noreply.github.com>
2019-03-10 19:25:06 -0400

    Update host-files.py

--- host-files.py
+++ host-files.py
@@ -35,14 +35,16 @@ class RootedHTTPRequestHandler(SimpleHTTPRequestHandler):
 
 def test(HandlerClass=RootedHTTPRequestHandler, ServerClass=RootedHTTPServer):
 
+    host_path='/root/host-files'
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', '-p', default=8000, type=int)
-    parser.add_argument('--dir', '-d', default='/root/host-files', type=str)
+    parser.add_argument('--dir', '-d', default=host_path, type=str)
     args = parser.parse_args()
 
-    if args.dir != '/root/host-files':
-        os.unlink('/root/host-files/pwd')
-        os.symlink(os.getcwd(), '/root/host-files/pwd')
+    if args.dir != host_path:
+        if os.path.isdir(host_path + '/pwd'):
+            os.unlink(host_path + '/pwd')
+        os.symlink(os.getcwd(), host_path + '/pwd')
 
     server_address = ('', args.port)
 
